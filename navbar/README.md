7430b65d3118729fb1be08b9b4c770e34d7f71ce
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-04-25 20:10:51 -0400

    Bug fixes

--- navbar/README.md
+++ navbar/README.md
@@ -6,7 +6,8 @@ An accessable and responsive navbar.
 
 ## To-Do:
 
+- [ ] If the window is resized when the menu is expanded, close the menu.
 - [ ] Add in current page indication html, css, and js
-- [ ] Change aria label on menu expand
+- [x] Change aria label on menu expand
 - [ ] Close menu when a link is clicked
 - [x] Add in responsiveness
--- navbar/js/main.js
+++ navbar/js/main.js
@@ -1,7 +1,10 @@
 const toggleMenu = () => {
+  const navbar = document.getElementsByClassName("navbar")[0];
   const btn = document.getElementById("menu-toggle");
   const menu = document.getElementById("navbarSupporedtContent");
   const navLinks = Array.from(document.getElementsByClassName("nav-link"));
+  const navItems = Array.from(document.getElementsByClassName("nav-item"));
+  let expandedHeight = navItems[0].offsetHeight * navItems.length + 50;
   const ariaExpand = btn.getAttribute("aria-expanded");
   let ariaState = "";
   let hiddenState = "";
@@ -10,20 +13,31 @@ const toggleMenu = () => {
     ariaState = "false";
     hiddenState = "true";
     tabIndex = "-1";
+    navbar.style.height = "50px";
   } else {
     ariaState = "true";
     hiddenState = "flase";
     tabIndex = "0";
+    navbar.style.height = `${expandedHeight}px`;
   }
   btn.classList.toggle("expanded");
   btn.setAttribute("aria-expanded", ariaState);
   menu.setAttribute("aria-hidden", hiddenState);
   menu.classList.toggle("collapsed");
+  navbar.classList.toggle("expanded");
   navLinks.forEach((link) => link.setAttribute("tabindex", tabIndex));
 };
 
+const screenCheck = () => {
+  if (screen.width < 800) {
+    const navLinks = Array.from(document.getElementsByClassName("nav-link"));
+    navLinks.forEach((link) => link.setAttribute("tabindex", "-1"));
+  } else {
+    const navbar = document.getElementsByClassName("navbar")[0];
+    navbar.style.height = "50px";
+  }
+};
+
+screenCheck();
 document.getElementById("menu-toggle").addEventListener("click", toggleMenu);
-if (screen.width < 800) {
-  const navLinks = Array.from(document.getElementsByClassName("nav-link"));
-  navLinks.forEach((link) => link.setAttribute("tabindex", "-1"));
-}
+window.onresize = screenCheck;
