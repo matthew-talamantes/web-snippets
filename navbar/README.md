039523f659918917e01f9dc2224002a16d99f75e
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-04-26 11:45:48 -0400

    Collapse menu on screen resize

--- navbar/README.md
+++ navbar/README.md
@@ -6,8 +6,8 @@ An accessable and responsive navbar.
 
 ## To-Do:
 
-- [ ] If the window is resized when the menu is expanded, close the menu.
+- [x] If the window is resized when the menu is expanded, close the menu.
 - [ ] Add in current page indication html, css, and js
 - [x] Change aria label on menu expand
-- [ ] Close menu when a link is clicked
+- [x] Close menu when a link is clicked
 - [x] Add in responsiveness
--- navbar/js/main.js
+++ navbar/js/main.js
@@ -1,37 +1,53 @@
-const toggleMenu = () => {
+const setMenuState = (state) => {
   const navbar = document.getElementsByClassName("navbar")[0];
   const btn = document.getElementById("menu-toggle");
   const menu = document.getElementById("navbarSupporedtContent");
   const navLinks = Array.from(document.getElementsByClassName("nav-link"));
   const navItems = Array.from(document.getElementsByClassName("nav-item"));
   let expandedHeight = navItems[0].offsetHeight * navItems.length + 50;
-  const ariaExpand = btn.getAttribute("aria-expanded");
   let ariaState = "";
   let hiddenState = "";
   let tabIndex = "";
-  if (ariaExpand === "true") {
+  if (state === "true") {
+    ariaState = "true";
+    hiddenState = "false";
+    tabIndex = "0";
+    navbar.style.height = `${expandedHeight}px`;
+    if (!btn.classList.contains("expanded")) {
+      btn.classList.add("expanded");
+      menu.classList.remove("collapsed");
+      navbar.classList.add("expanded");
+    }
+  } else {
     ariaState = "false";
     hiddenState = "true";
     tabIndex = "-1";
     navbar.style.height = "50px";
-  } else {
-    ariaState = "true";
-    hiddenState = "flase";
-    tabIndex = "0";
-    navbar.style.height = `${expandedHeight}px`;
+    if (btn.classList.contains("expanded")) {
+      btn.classList.remove("expanded");
+      menu.classList.add("collapsed");
+      navbar.classList.remove("expanded");
+    }
   }
-  btn.classList.toggle("expanded");
+
   btn.setAttribute("aria-expanded", ariaState);
   menu.setAttribute("aria-hidden", hiddenState);
-  menu.classList.toggle("collapsed");
-  navbar.classList.toggle("expanded");
   navLinks.forEach((link) => link.setAttribute("tabindex", tabIndex));
+};
+
+const toggleMenu = () => {
+  const btn = document.getElementById("menu-toggle");
+  const ariaExpand = btn.getAttribute("aria-expanded");
+  if (ariaExpand === "true") {
+    setMenuState("false");
+  } else {
+    setMenuState("true");
+  }
 };
 
 const screenCheck = () => {
   if (screen.width < 800) {
-    const navLinks = Array.from(document.getElementsByClassName("nav-link"));
-    navLinks.forEach((link) => link.setAttribute("tabindex", "-1"));
+    setMenuState("false");
   } else {
     const navbar = document.getElementsByClassName("navbar")[0];
     navbar.style.height = "50px";
